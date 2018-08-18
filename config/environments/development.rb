97d4d0468a0feec80558a565cf840d5b86c85875
Ekaterina Gorbacheva <ekagorbacheva@mail.ru>
2018-08-19 00:44:46 +0800

    config eager-load

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,7 @@ config.webpacker.check_yarn_integrity = true
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = false
+  config.assets.debug = true
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
