215ee8e51a7c79e378dbb44282b2598f7aee6c01
Ekaterina Gorbacheva <ekagorbacheva@mail.ru>
2018-08-20 00:40:34 +0800

    migrate

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
     resources :comments, only: [ :index, :show ]
   root to: 'comments#index'
+  # resources :comments, :only [ :show, :edit, :update, :destroy ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
